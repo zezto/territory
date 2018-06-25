37d8f2f0399863cbd214cec7dedbf9ca0fbe4d91
zezto <31606201+zezto@users.noreply.github.com>
2018-06-25 18:17:45 -0500

    good commit <3

--- main/models.py
+++ main/models.py
@@ -36,7 +36,7 @@ class Number(models.Model):
     def __str__(self):
         return str(self.value)"""
 
-
+'''
 class Number(models.Model):
     value = models.IntegerField(default=0)
     visit1 = models.CharField(max_length=100, blank=True)
@@ -70,3 +70,34 @@ class Terr(models.Model):
         return str(self.num) + ' ' + self.sub + ' ' + self.Owner
 
         #try many to one relationship now lol 
+        #also try rverse relation too accoridn to your stackoverflow
+        #many to one now lol
+'''
+class Terr(models.Model):
+    num = models.IntegerField(default=0)
+    sub = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    lat_cordinate = models.FloatField(default=0)
+    long_cordinate = models.FloatField(default=0)
+
+    def __str__(self):
+        return str(self.num) + ' ' + self.sub + ' ' + self.owner
+
+class Street(models.Model):
+    name = models.CharField(max_length=100)
+    date_worked = models.DateField(auto_now=True)
+    terr = models.ForeignKey(Terr, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Number(models.Model):
+    value = models.IntegerField(default=0)
+    visit1 = models.CharField(max_length=100, blank=True)
+    visit2 = models.CharField(max_length=100, blank=True)
+    visit3 = models.CharField(max_length=100, blank=True)
+    notes = models.CharField(max_length=100, blank=True)
+    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.value)
