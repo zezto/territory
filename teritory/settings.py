cc565240bfa02b2f31a2b7e2b020b4f57c431062
zezto <zeztomstwnt@gmail.com>
2019-04-08 22:09:58 -0500

    fix master

--- teritory/settings.py
+++ teritory/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'teritory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-	'NAME': 'jardines',
-	'USER': 'jardinesuser',
-	'PASSWORD':'jardinespassword',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
