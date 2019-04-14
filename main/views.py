726f243c1a8175d26a25c1636bc0cf89c69315e1
zezto <zeztomstwnt@gmail.com>
2019-04-14 15:09:08 -0500

    fixed date and css for server

--- main/views.py
+++ main/views.py
@@ -45,17 +45,22 @@ def create_post(request, pk, streetpk):
         try:
             for num in result:
                 if len(result[num]) != 0:
-                    print(num)
                     if num == '0':
                         selectNumber.visit1 = result[num]
+                        selectNumber.date_worked1 = str(datetime.date.today().strftime('%b %d,%Y'))
                     if num == '1':
                         selectNumber.visit2 = result[num]
+                        selectNumber.date_worked2 = str(datetime.date.today().strftime('%b %d,%Y'))
+
+
                     if num == '2':
                         selectNumber.visit3 = result[num]
+                        selectNumber.date_worked3 = str(datetime.date.today().strftime('%b %d,%Y'))
+
+
                     if num == '3':
                         selectNumber.notes = result[num]
                     selectNumber.save()
-
         except KeyError:
             print('error key')
         return HttpResponse(
@@ -82,7 +87,7 @@ def detail(request, pk):
             box_size=10,
             border=4,
         )
-        data = 'http://207.148.0.247/%s/ ' % pk
+        data = '10.0.0.4:8000/%s/ ' % pk
         qr.add_data(data)
         qr.make(fit=True)
         img = qr.make_image()
@@ -98,6 +103,7 @@ def detail(request, pk):
 def streetdeets(request, pk, streetpk):
     street = Street.objects.get(pk=streetpk)
     form = VisitForm()
+    first_num = street.number_set.all().first()
     pathtostreetqr = Path(QR_ROOT + 'street_%s' % streetpk)
     if pathtostreetqr.is_file():
         pass
@@ -108,14 +114,14 @@ def streetdeets(request, pk, streetpk):
             box_size=10,
             border=4,
         )
-        data = 'http://207.148.0.247/%s/%s ' % (pk, streetpk)
+        data = '10.0.0.4:8000/%s/%s ' % (pk, streetpk)
         qr.add_data(data)
         qr.make(fit=True)
         img = qr.make_image()
         final = QR_ROOT + 'street-%s.jpeg' % streetpk
         img.save(final)
 
-    return render(request, 'html/street_details.html', {'street': street, 'form': form})
+    return render(request, 'html/street_details.html', {'street': street,'f':first_num, 'form': form})
 
 
 def spliter(input):
